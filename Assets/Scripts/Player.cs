8da0cf3ce1a5c3690d795428cfe85205704481fd
cross <ne280107@senshu-u.jp>
2019-03-06 02:08:23 +0900

    取り敢えずステージとプレイヤーの仮置きと、プレイヤーの移動の実装

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField]
+	private float speed = 0.25f;
+	private Vector3 movePosition;
+
+	private void Start()
+	{
+		movePosition = this.transform.position;
+	}
+
+	private void Update()
+	{
+		// プレイヤーの移動
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			movePosition.z += speed;
+			this.transform.position = movePosition;
+		}
+		
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			movePosition.x -= speed;
+			this.transform.position = movePosition;
+		}
+		
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			movePosition.z -= speed;
+			this.transform.position = movePosition;
+		}
+		
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			movePosition.x += speed;
+			this.transform.position = movePosition;
+		}
+	}
+}
