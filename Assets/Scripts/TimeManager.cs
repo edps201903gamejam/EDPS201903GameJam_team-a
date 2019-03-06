193429d68be2b333a1fdbfa389eb6a09543fcac1
mtsgi <oq@live.jp>
2019-03-06 16:53:18 +0900

    制限時間(TimeManager)の実装

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimeManager : MonoBehaviour {
+
+	public float timelimit;
+
+	private float left = 0f;
+	private float remain = 0f;
+
+	[SerializeField]
+	private UnityEngine.UI.Text TimeText;
+
+	void Update () {
+		left += Time.deltaTime;
+		remain = timelimit - left;
+		TimeText.text = Mathf.Floor(remain).ToString();
+
+		if ( remain <= 0 ) {
+			SceneManager.LoadScene(sceneName: "ResultScene");
+		}
+	}
+}
