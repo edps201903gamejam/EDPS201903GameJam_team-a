e90f90671085266d9c910ac24e5bd032ba3612b7
mtsgi <oq@live.jp>
2019-03-06 11:58:01 +0900

    ScoreManagerとUIManagerを作った

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+	public int scoreA = 0;
+	public int scoreB = 0;
+
+	private void Update(){
+		scoreA += Random.Range(0, 3);
+		scoreB += Random.Range(0, 10);
+	}
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+	public GameObject Player;
+	public GameObject MinimapDot;
+	public float moveSpeed = 10;
+
+	public ScoreManager ScoreManager;
+	public UnityEngine.UI.Text ScoreTextA;
+	public UnityEngine.UI.Text ScoreTextB;
+
+	// Update is called once per frame
+	void Update(){
+		//ミニマップのドット移動
+		float mapX = (Player.transform.position.x * moveSpeed) + 85;
+		float mapZ = (Player.transform.position.z * moveSpeed) + 70;
+		MinimapDot.transform.position = new Vector3(mapX, mapZ, 0);
+
+		//スコア表示
+		ScoreTextA.text = "A：" + ScoreManager.scoreA;
+		ScoreTextB.text = "B：" + ScoreManager.scoreB;
+	}
+}
