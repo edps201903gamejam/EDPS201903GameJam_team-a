781c91a20f2b412d8720b0a984f37a9ebe07180a
Taisei Nakajima <ne300085@senshu-u.jp>
2019-03-07 14:35:01 +0900

    ガバだけどNPCできた

--- Assets/NPCMove.cs
+++ Assets/NPCMove.cs
@@ -18,7 +18,7 @@ public class NPCMove : MonoBehaviour
 	private Vector3 npcPos;
 
 	private float angle = 30.0f;
-	private float distance = 10.0f;
+	private float distance = 3.0f;
 
 	public GameObject player;
 	public float speed = 0.1f;
@@ -42,24 +42,25 @@ public class NPCMove : MonoBehaviour
 		playerPos = player.transform.position;
 		npcPos = transform.position;
 		
-			Debug.Log(Vector3.Angle((playerPos - npcPos).normalized, eyeDir));
-			if (Vector3.Angle((playerPos - npcPos).normalized, eyeDir) <= angle
-			    && Vector3.Distance( ))
-			    
-			{
-				ChasePlayer();
-			}
-			else if(!agent.pathPending && agent.remainingDistance < 0.5f)
-			{
-				GotoNextPoint();
-			}
+//		Debug.Log(Vector3.Angle((npcPos - playerPos).normalized, eyeDir));
+		if (Vector3.Angle((npcPos - playerPos).normalized, eyeDir) <= angle
+		    && Vector3.Distance(npcPos, playerPos) <= distance
+		    && Physics.Linecast(npcPos, playerPos))
+		{
+			ChasePlayer();
+		}
+		else if(!agent.pathPending && agent.remainingDistance < 1.0f)
+		{
+			GotoNextPoint();
+		}
+			
 	}
 	
 	void GotoNextPoint()
 	{
 		if (points.Length == 0)
 			return;
-		
+		Debug.Log("巡回中……");
 		agent.destination = points[destPoint].position;
 		destPoint = (destPoint + 1) % points.Length;
 	}
@@ -68,8 +69,7 @@ public class NPCMove : MonoBehaviour
 	{
 		Debug.Log("chase中……");
 		transform.rotation = Quaternion.Slerp(transform.rotation,
-			              Quaternion.LookRotation(playerPos - transform.position), 0.5f);
-
-		transform.position += transform.forward * speed;
+			              Quaternion.LookRotation(playerPos - transform.position), 0.01f);
+		transform.position += transform.forward * speed * 0.1f;
 	}
 }
